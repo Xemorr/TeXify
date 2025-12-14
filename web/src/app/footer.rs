df0b1f21954c1e3fa80c8fc8c6fe7f175ae34881
Xemor <xemor@xemor.me>
2025-12-14 23:39:35 +0000

    Nice FAQ, probably installable as PWA?

--- web/src/app/footer.rs
+++ web/src/app/footer.rs
@@ -1,4 +1,4 @@
-use leptos::prelude::ElementChild;
+use leptos::prelude::{ElementChild, StyleAttribute};
 use leptos::{component, IntoView};
 use leptos::prelude::ClassAttribute;
 use crate::app::view;
@@ -6,11 +6,12 @@ use crate::app::view;
 #[component]
 pub fn footer() -> impl IntoView {
     view! {
-        <div class="container">
-            <p>"Credit to the " <a href="https://detexify.kirelabs.org/classify.html">"original detexify app"</a>", their data was used to train this classifier."</p>
-            <br/>
-            <h2>"Why use this version?"</h2>
-            <p>"It performs classification locally, so performs faster, can be installed as an app on any device and uses modern ML techniques (CNN)"</p>
+        <div class="footer">
+            <h1>"FAQ"</h1>
+            <h2>"Inspiration & Credit"</h2>
+            <p>"Credit to the " <a href="https://detexify.kirelabs.org/classify.html">"original detexify app"</a> ", their data was used to train this classifier."</p>
+            <h2>"How does this app work?"</h2>
+            <p>"It's written in Rust using Leptos, compiled to web assembly in order to have great performance running a ML model from the browser. This leads to lower latency, and if you install this as an app, you can continue to use it even if this website goes down!"</p>
         </div>
     }
 }
