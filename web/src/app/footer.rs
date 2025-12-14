18bde8a89a8163fe1f69292f2af515d41c745214
Xemor <xemor@xemor.me>
2025-12-14 20:28:10 +0000

    Improve footer@

--- web/src/app/footer.rs
+++ web/src/app/footer.rs
@@ -7,8 +7,10 @@ use crate::app::view;
 pub fn footer() -> impl IntoView {
     view! {
         <div class="container">
-            <p>"Credit to the " <a href="https://detexify.kirelabs.org/classify.html">original detexify app</a>", their data was used to train this classifier."</p>
-            <p>"Why use this version? It performs classification locally, so performs faster, can be installed as an app on any device and uses modern ML techniques (CNN)"</p>
+            <p>"Credit to the " <a href="https://detexify.kirelabs.org/classify.html">"original detexify app"</a>", their data was used to train this classifier."</p>
+            <br/>
+            <h2>"Why use this version?"</h2>
+            <p>"It performs classification locally, so performs faster, can be installed as an app on any device and uses modern ML techniques (CNN)"</p>
         </div>
     }
 }
